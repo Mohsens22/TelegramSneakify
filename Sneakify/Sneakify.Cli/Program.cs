a4c27e567bbbd0df946748abe3c5764b32f56fe5
Mohsen Seifi <mohsens2@outlook.com>
2019-07-21 21:35:55 +0430

    init

---
+++ Sneakify/Sneakify.Cli/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sneakify.Cli
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var result = RegisterCommands(args);
+#if DEBUG
+            WaitForExit();
+#endif
+            return result;
+        }
+
+        private static int RegisterCommands(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WaitForExit()
+        {
+            Console.WriteLine("Press enter key to exit");
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+}
