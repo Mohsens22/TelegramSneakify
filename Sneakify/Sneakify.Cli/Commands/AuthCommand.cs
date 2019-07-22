6c0087e707708581e474795f36f39e458e2b2212
Mohsen Seifi <mohsens2@outlook.com>
2019-07-22 08:55:16 +0000

    Added auth

---
+++ Sneakify/Sneakify.Cli/Commands/AuthCommand.cs
@@ -0,0 +1,17 @@
+﻿using ManyConsole;
+using Sneakify.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sneakify.Cli.Commands
+{
+    class AuthCommand : ConsoleCommand
+    {
+        public override int Run(string[] remainingArguments)
+        {
+            AuthHandler.Run();
+            return 1;
+        }
+    }
+}
---
+++ Sneakify/Sneakify/Handlers/AuthHandler.cs
@@ -0,0 +1,12 @@
+﻿using Sneakify.Telegram;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sneakify.Handlers
+{
+    public static class AuthHandler
+    {
+        public static void Run() => new Auth().Run();
+    }
+}
